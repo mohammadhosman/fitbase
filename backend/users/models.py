e868c7f86634ca303a53f34bac69e1687e52f3a9
mohammadosman <mohammadhakimosman@outlook.com>
2025-07-31 22:27:54 -0700

    created custom User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True, max_length=254)
+    #password
+    first_name = models.CharField(max_length=50)
+
+    #Additional profile and personal info
+    #Might creae a separate UserProfile model and create a one-to-one relationship
+    #between User and UserProfile
+    last_name = models.CharField(max_length=50, 
+                                blank=True, null=True)
+    #Using regex to validate phone numbers entered when a user creates an account
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    #Might use "django-phonenumber-field" package instead. Still a maybe though becuase it's a big package
+    #Source: https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-a-phone-number-in-django-models
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, null=True)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    
+    bio = models.CharField(max_length=600)
+
+    USERNAME_FIELD = 'username'
